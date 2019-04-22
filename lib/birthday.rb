81164e9f03c2bab8c71fe79931432c566b002aae
Ricky Rojas <rickyrojas19@gmail.com>
2019-04-22 19:36:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,8 +15,5 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kid, age|
     age <= 12 ? puts "Happy Birthday #{kid}! You are now #{age} years old!" : puts "You are too old for this."
-  end
-  end
-  end
 end
 
